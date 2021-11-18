ddc5ec1ba9fdad1f3e9cedfd5091f47fedcf15d3
pexugadepollo <bso080601@gmail.com>
2021-11-18 10:59:57 +0100

    neverused
    Signed-off-by: pexugadepollo <bso080601@gmail.com>

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,4 @@
 import NavBar from "../../components/NavBar";
-import {Carousel} from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Main = () => {
